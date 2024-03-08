1fcc27df76b1045c2a281f27f6a777e128f62b96
howly <94230221+howlysolid@users.noreply.github.com>
2024-03-08 16:39:08 +0100

    classe "Griglia"

---
+++ Griglia.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+//#include <ncurses.h>
+using namespace std;
+
+#define ROW 23
+#define COL 12
+
+class Griglia {
+  protected:
+    char matrice[ROW][COL];
+  public:
+    void inizializzazione();
+    void stampa();
+    bool controllo_ultima_riga();
+    void scorrimento();
+    void collisioni();
+    void game_over();
+};
