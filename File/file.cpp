3398de30a9cd5ec4b106390cae620e4cf857f244
howly <94230221+howlysolid@users.noreply.github.com>
2024-03-11 18:00:13 +0100

    Update file.cpp

--- File/file.cpp
+++ File/file.cpp
@@ -1,4 +1,64 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 #include "file.hpp"
+
+// struct e typedef da mettere in .hpp
+
+struct node {
+  int val;
+  node* next;
+};
+
+typedef node* plist;
+
+void save_data(int points) {
+  ofstream file;
+  file.open("classifica.txt", ios::app);
+
+  file << points;
+  file.close();
+}
+
+void load_data() {
+  ifstream file;
+  file.open("classifica.txt");
+  int pointse;
+  plist head = NULL;
+  
+  while (file >> points) {
+    addNode(points);
+  }
+}
+
+plist addNode(int points) {
+  if (head == NULL) {
+    head = new node;
+    head->val = value;
+    head->next = NULL;
+    return;
+  }
+
+  bool added = false;
+  plist curr = head;
+
+  while (!added && curr != NULL) {
+    if (curr->val < points) {
+      plist tmp = curr->next;
+      curr->next = new node;
+      (curr->next)->val = points;
+      (curr->next)->next = tmp;
+      added = true;
+    }
+
+    curr = curr->next;
+  }
+
+  if (!added) {
+    plist n = new node;
+    n->val = points;
+    n->next = head;
+    head = n;
+  }
+}
