97c7423be4cbaf6c22dbc535fa924dbac37d7658
CesidioR <161586794+CesidioR@users.noreply.github.com>
2024-04-08 11:01:44 +0200

    Add files via upload

--- Tetramini/Tetramini.hpp
+++ Tetramini/Tetramini.hpp
@@ -1,6 +1,7 @@
 #include <cstring>
 #include <ncurses.h>
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
@@ -12,11 +13,9 @@ class Tetramini
 {
 protected:
     int xloc, yloc, xmax, ymax;
-    char tetram[R][C];
-    char l[L];
+    int cur_rotation;
     WINDOW *curwin;
 public:
-
     Tetramini(){
         getmaxyx(curwin,ymax,xmax);
     }
@@ -67,10 +66,40 @@ public:
         int x=xloc;
         for (int i = 0; i < L; i++)
         {
-            l[i]=' ';
-            if(i%2==0 && i!=0) y++, x=xloc;
-            mvwaddch(curwin, y, x, l[i]);
-            x++;
+        if(cur_rotation==90){
+            if (i % 2 == 0) {
+                if (i != 0) {
+                    y++;
+                    x = xloc;
+                }
+               
+                } 
+                mvwaddch(curwin, y, x, ' ');
+                x++;
+            }
+            else if (cur_rotation == 180) {
+                if(i==0) y=y+2, x=x-2;
+                mvwaddch(curwin, y, x, ' ');
+                x++;
+                
+            } else if (cur_rotation == 270) {
+                if(i==0){ 
+                    x=x+2;
+                }
+                if (i % 2 == 0) {
+                if (i != 0) {
+                    y++;
+                    x = xloc+2;
+                }
+                }
+                mvwaddch(curwin, y, x, ' ');
+                x++;
+            } else if (cur_rotation == 0) {
+                if(i==0) y++, x=x-2;
+                mvwaddch(curwin, y, x, ' ');
+                x++;
+                
+            }
         }
     }
 
@@ -81,14 +110,23 @@ public:
         {
             for (int j = 0; j < C; j++)
             {
-                tetram[i][j]=' ';
-                mvwaddch(curwin,y,x, tetram[i][j]);
+                mvwaddch(curwin,y,x, ' ');
                 x++;
                 if(j==3) x=xloc, y++;
             }
             
         }
     }
+
+   /* void get_tetram(){
+        srand(time(0));
+        int a= rand()%2;
+        if (a==0)
+        {
+            Quadrato(curwin, 0,5);
+        }
+        else Linea (curwin, 0,6);
+    }*/
 };
 
 
@@ -103,39 +141,22 @@ class Quadrato : public Tetramini{
         getmaxyx(curwin,ymax,xmax);
         yloc=y;
         xloc=x;
-        for (int  i = 0; i < R; i++)
-        {
-            for (int j = 0; j < C; j++)
-            {
-                if(j%2==0){
-                    tetram[i][j]='[';
-                }
-                else tetram[i][j]=']';
-                mvwaddch(curwin,y,x, tetram[i][j]);
-                x++;
-                if(j==3) x=xloc, y++;
-            }
-            
-        }
+        display();
     };
 
     void mvdown(){
         empty1();
-        yloc++;
-        if (yloc>ymax-3) yloc=ymax-3;
-
+        if (yloc<ymax-3) yloc++;
     }
 
     void mvleft(){
         empty1();
-        xloc--;
-        if(xloc<1) xloc=1;
+        if(xloc>1) xloc--;
     }
 
     void mvright(){
         empty1();
-        xloc++;
-        if(xloc>xmax-5) xloc=xmax-5;
+        if(xloc< xmax-5) xloc++;;
     }
 
     int getmv(){
@@ -165,10 +186,9 @@ class Quadrato : public Tetramini{
             for (int j = 0; j < C; j++)
             {
                 if(j%2==0){
-                    tetram[i][j]='[';
+                    mvwaddch(curwin,y,x, '[');
                 }
-                else tetram[i][j]=']';
-                mvwaddch(curwin,y,x, tetram[i][j]);
+                else mvwaddch(curwin,y,x, ']');
                 x++;
                 if(j==3)x=xloc, y++;
             }
@@ -180,72 +200,132 @@ class Quadrato : public Tetramini{
 
 class Linea : public Tetramini{
     protected:
-    
     public:
-    Linea(WINDOW *win,int y, int x){
+    Linea (WINDOW *win,int y, int x){
         curwin=win;
         keypad(curwin, true);
         getmaxyx(curwin,ymax,xmax);
         yloc=y;
         xloc=x;
-        for (int i = 0; i < L; i++)
-        {
-            if(i%2==0) l[i]='[';
-            else l[i]=']';
-            if(i%2==0 && i!=0) y++, x=xloc;
-            mvwaddch(curwin, y, x, l[i]);
-            x++;
-        }
+        cur_rotation=90;
+        display();
     }
     
     void mvdown(){
         empty2();
-        yloc++;
-        if (yloc>ymax-5)
-        {
-            yloc=ymax-5;
+        if((cur_rotation==90 || cur_rotation==270) && yloc<ymax-5){
+            yloc++;
         }
-        
+        else if (cur_rotation == 180 && yloc<ymax-4) {
+            yloc++;    
+        } else if (cur_rotation == 0 && yloc<ymax-3) {
+               yloc++;
+        }
+    }
+
+    void mvright(){
+        empty2();
+        if(cur_rotation==90 && xloc<xmax-3){
+            xloc++;
+        }
+        else if ((cur_rotation == 180 || cur_rotation==0) && xloc<xmax-7) {
+            xloc++;    
+        } else if (cur_rotation == 270 && xloc<xmax-5) {
+               xloc++;
+        }
+    }
+
+     void mvleft(){
+        empty2();
+        if(cur_rotation==90 && xloc>1){
+            xloc--;
+        }
+        else if ((cur_rotation == 180 || cur_rotation==0) && xloc>3) {
+            xloc--;    
+        } else if (cur_rotation == 270 && xloc>-1) {
+               xloc--;
+        }
     }
 
     void rotation(){
         empty2();
-        int y=yloc+1;
-        int x=xloc;
-        for (int i = 0; i < L; i++)
-        {
-            if(i%2==0) l[i]='[';
-            else l[i]=']';
-            mvwaddch(curwin, y, x, l[i]);
-            x++;
-        }
+        int rotation_angle= (cur_rotation+90) % 360;
+        cur_rotation= rotation_angle;
+       
     }
 
-    void display1(){
-        int y=yloc;
-        int x=xloc;
-        for (int i = 0; i < L; i++)
-        {
-            if(i%2==0) l[i]='[';
-            else l[i]=']';
-            if(i%2==0 && i!=0) y++, x=xloc;
-            mvwaddch(curwin, y, x, l[i]);
-            x++;
+    void display(){
+        int y = yloc;
+        int x = xloc;
+        for (int i = 0; i < L; i++) {
+            if(cur_rotation==90){
+            if (i % 2 == 0) {
+                if (i != 0) {
+                    y++;
+                    x = xloc;
+                }
+                mvwaddch(curwin, y, x, '[');
+                } else {
+                    mvwaddch(curwin, y, x, ']');
+                }
+                x++;
+            }
+            else if (cur_rotation == 180) {
+                if(i==0) y=y+2, x=x-2;
+                if (i%2==0)
+                {
+                    mvwaddch(curwin, y, x, '[');
+                }
+                else mvwaddch(curwin, y, x, ']');
+                x++;
+                
+            } else if (cur_rotation == 270) {
+                if(i==0){ 
+                    x=x+2;
+                }
+                if (i % 2 == 0) {
+                if (i != 0) {
+                    y++;
+                    x = xloc+2;
+                }
+                mvwaddch(curwin, y, x, '[');
+                } else {
+                    mvwaddch(curwin, y, x, ']');
+                }
+                x++;
+            } else if (cur_rotation == 0) {
+                if(i==0) y++, x=x-2;
+                if (i%2==0)
+                {
+                    mvwaddch(curwin, y, x, '[');
+                }
+                else mvwaddch(curwin, y, x, ']');
+                x++;
+                
+            }
         }
-    }    
-
+    };  
+    
     int getmv(){
         int choice = wgetch(curwin);
         switch (choice)
         {
-        case KEY_DOWN:
+         case KEY_DOWN:
             mvdown();
             break;
-        default:
+         case KEY_LEFT:
+            mvleft();
+            break;
+         case KEY_RIGHT:
+            mvright();
+            break;
+         case KEY_UP:
+            rotation();
+            break;
+         default:
             break;
         }
     return choice;
     }
 
 };
-
